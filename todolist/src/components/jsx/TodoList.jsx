e5c132d8f124a3760766c1f10faef8451ddd3fee
Tomas1876 <mnd3259@gmail.com>
2021-07-18 01:18:13 +0900

    todolist 만들기

---
+++ todolist/src/components/jsx/TodoList.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import '../css/TodoList.css';
+
+const TotoList = ({form, children}) =>{
+    return(
+        <>
+        <main className='todo-list-template'>
+            <div className='todo-list-title'>
+                오늘 할 일
+            </div>  
+            <section className='form-wrapper'>
+                {form}    
+            </section>
+            <section className='todoitemList-wrapper'>
+                {children}
+            </section> 
+        </main>
+        </>
+    );
+};
+
+export default TodoList;
