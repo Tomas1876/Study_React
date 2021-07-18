49c7900b174033a2ac1ae5d8e9e91ad9e8e5f45a
Tomas1876 <mnd3259@gmail.com>
2021-07-18 23:02:40 +0900

    Array.from과 Object.assign

--- codingeverybody_react_create/src/App.js
+++ codingeverybody_react_create/src/App.js
@@ -36,17 +36,13 @@ class App extends Component{
 
   onSubmitCreate =(_title, _desc) =>{
     console.log(_title, _desc);
-    // this.state.contents.push({id:this.state.contents.id + 1, title:_title, desc:_desc });
-    // this.setState({
-    //     contents:this.state.contents  
-    // });
     var max = this.state.max_id;
-    var _contents = this.state.contents.concat({id:max + 1, title:_title, desc:_desc});
+    // var _contents = this.state.contents.concat({id:max + 1, title:_title, desc:_desc});
     
-    this.setState({
-      contents:_contents,
-      max_id:max +1
-    });
+    // this.setState({
+    //   contents:_contents,
+    //   max_id:max +1
+    // });
 
     //배열에 값을 추가하는 방법은 push와 concat 두 가지가 있으나
     //push는 원본을 바꾼다(arr =[2,3]일때 arr.push(1) 이면 arr 원본에 1을 추가하는 것)
@@ -54,6 +50,18 @@ class App extends Component{
     //var arr2 = arr.concat(1)이면 arr은 여전히 [2,3]이고
     //concat이 리턴한 arr2가 [2,3,1]이다
     //리액트의 성능을 개선하기 위해서는 push보다 concat이 더 적합하다
+
+    //concat말고 from도 배열을 복사할 수 있다
+    //만약 배열 아니라 객체를 복사해서 리턴하고 싶다면 Object.assign()을 사용할 수 있다
+    //예를 들어 var a = {name:a} 일때 var b = Object.assign({},a) 로 빈 객체를 추가하더라도
+    // a == b는 false가 된다 만약 var b =  Object.assign({name2:b},a) 라고 하면
+    //var b는 {name2:b, name:a}인 객체가 된다
+    
+    var newContents = Array.from(this.state.contents);
+    newContents.push({id:max + 1, title:_title, desc:_desc});
+    this.setState({
+      contents:newContents //기존의 배열을 복사한 것을 리턴한 newContents에 push 했으니 원본은 그대로!
+    })
   }
   render(){
     console.log('App render');
