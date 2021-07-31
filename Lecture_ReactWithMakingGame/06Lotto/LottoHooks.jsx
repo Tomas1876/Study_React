2a9bba8474ae03c5f82ae30386c61a1f2a397adb
Tomas1876 <mnd3259@gmail.com>
2021-07-31 19:09:38 +0900

    useMemo, useCallback 그리고 useEffect 쪼개서 사용하기

--- Lecture_ReactWithMakingGame/06Lotto/LottoHooks.jsx
+++ Lecture_ReactWithMakingGame/06Lotto/LottoHooks.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useRef} from 'react';
+import React, {useState, useEffect, useRef, useMemo, useCallback} from 'react';
 import Ball from './Ball';
 
 
@@ -16,11 +16,21 @@ function getWinNumbers(){
 } 
 
 const LottoHooks = () =>{
-    const [winNumbers, setWinNumbers] = useState(getWinNumbers());
+    const lottoNumbers = useMemo(()=>getWinNumbers(),[]);
+    //useEffect나 useMemo 등은 늘 두 번째 인자로 배열을 가지는데
+    //배열 안의 값이 변화하지 않는 이상 다시 실행되지 않는다
+    //이렇게 useMemo를 사용해서 값을 기억한 다음 그 값을 winNumber에 넣어주어야
+    //getWinNumber가 Balls 컴포넌트가 렌더링 될 때마다 재실행되지 않는다
+    //useMemo는 함수의 실행 결과값 등을 기억하고
+    //useRef는 비교적 간단한 일반 값을 기억한다
+
+    //반면 useCallBack은 함수의 결과값이 아니라 함수 자체를 기억한다
+    const [winNumbers, setWinNumbers] = useState(lottoNumbers);
     const [winBalls, setWinBalls] = useState([]);
     const [bonus, setBonus] = useState(null);
     const [redo, setRedo] = useState(false);
     const timeouts = useRef([]);
+    //Hooks는 선언 순서도 중요하다 실행순서에 맞춰 선언해야 함
     
     useEffect(()=>{
         console.log("useEffect")
@@ -34,6 +44,24 @@ const LottoHooks = () =>{
         // useEffect는 componentDidMount와 동일한 역할을 한다
         // 만약 이 부분이 빈 배열이 아니라면 useEffect는
         // componentDidMount와 componentDidupdate 둘 다 수행
+
+        // useEffect는 componentDidMount, componentDidUpdate의 역할을 하기 때문에
+        // componenteDidMout의 역할을 하지 않고 componentDidUpdate만 하길 원한다면, 아래와 같이 할 수 있다
+        const mounted = useRef(false);
+        useEffect(()=>{
+            if(!mounted.current){
+                mounted.current = true; //이렇게 하면 componenteDidMout 시점에 아무일도 일어나지 않는다
+            } else{
+                //여기에 update시 실행할 방법을 넣는다
+            }
+        },[]); //이 배열에 바뀌는 값을 넣는다
+
+        //만약 componentDidUpdate말고 componenteDidMout만 하고 싶다면
+        useEffect(()=>{
+            //실행할 동작
+        },[]);
+
+        //위 두 개는 일종의 요령인데 패턴이니까 기억해둘 것
 
     const runTimeouts = ()=>{
 
@@ -54,15 +82,21 @@ const LottoHooks = () =>{
 
     }
 
-    const onClickRedo =() =>{ //한 번 더 할때 초기화 시켜주는 함수
+    const onClickRedo = useCallback(() => {
+        console.log('onClickRedo');
+        console.log(winNumbers);
         setWinNumbers(getWinNumbers());
         setWinBalls([]);
         setBonus(null);
         setRedo(false);
-
         timeouts.current = [];
-        
-    }
+      }, [winNumbers]);
+    //함수컴포넌트는 전체가 재실행된다는 특징이 있는데
+    //useCallBack으로 함수를 기억해두면 재실행 될 때마다 함수를 불필요하게 새로 생성할 필요가 없다
+    //만약 자식 컴포넌트에 함수를 props로 넘긴다면 useCallback을 반드시 설정해야 한다
+    //그렇게 하지 않으면 함수형 부모 컴포넌트가 새로 렌더링 될 때마다 함수는 바뀐 것이 없는데
+    //함수가 새로 렌더링 됐기 때문에 props가 바뀌었다고 생각해 자식 컴포넌트도 새로 렌더링이 된다
+    //불필요한 렌더링 줄줄이 발생
 
     return(
         <>
